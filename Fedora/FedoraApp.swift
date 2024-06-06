2c2380a5ecfca62f22b6b4fb191a5063500ade2a
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-06-06 15:16:57 +0200

    Initial Commit

---
+++ Fedora/FedoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FedoraApp.swift
+//  Fedora
+//
+//  Created by SHOHJAHON on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FedoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
